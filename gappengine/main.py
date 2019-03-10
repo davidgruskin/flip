0d9e35bd82cb3d166fc0b9ed8c1d358ac2c302f8
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-09 22:24:27 -0500

    added jsonify

--- gappengine/main.py
+++ gappengine/main.py
@@ -43,7 +43,7 @@ def getData():
     data = request.get_json()
     for x in request.args:
     	if "mydata" in x:
-    		print("MY DATA: %s" % (x["mydata"]))
+    		print("MY DATA: %s" % ( x.get("mydata", "NOTHING") ))
     	print("LOST DATA: %s" % (x))
     arr = {}
     arr["blah"] = []
