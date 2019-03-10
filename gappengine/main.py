f8b8bba035d2f9cfe5e42fc4f319509e7ff588f5
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-09 21:07:46 -0500

    added flask cors

--- gappengine/main.py
+++ gappengine/main.py
@@ -1,13 +1,20 @@
 from flask import Flask
 
+# pip install -U flask-cors
+from flask_cors import cross_origin
+
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin(send_wildcard=True)
 def hello():
+	self.response.headers['Access-Control-Allow-Origin'] = '*'
     return 'This is just a test!'
 
 @app.route('/curated', methods=['POST'])
+@cross_origin(send_wildcard=True)
 def curated():
+	self.response.headers['Access-Control-Allow-Origin'] = '*'
 	print("curated: request received")
 	data = request.get_json()
 	print(data)
