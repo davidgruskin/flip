7363b3f02cc518ad4841fd31506116840c69871a
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-09 22:07:50 -0500

    added jsonify

--- gappengine/main.py
+++ gappengine/main.py
@@ -1,9 +1,25 @@
-from flask import Flask, jsonify, request
+from functools import wraps
+from flask import Flask, request, current_app, jsonify
+
 
 # pip install -U flask-cors
 from flask_cors import cross_origin
 
 app = Flask(__name__)
+
+def support_jsonp(f):
+    """Wraps output to JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        result = jsonify(f(*args, **kwargs))
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(result.data) + ')'
+            return current_app.response_class(content,
+                                              mimetype='application/json')
+        else:
+            return result
+    return decorated_function
 
 @app.route('/')
 @cross_origin(send_wildcard=True)
@@ -21,6 +37,7 @@ def curated():
 
 @app.route('/_getData', methods=['GET', 'POST'])
 @cross_origin(send_wildcard=True)
+@support_jsonp
 def getData():
     #hit the data, package it, put it into json.
     #ajax would have to hit this every so often to get latest data.
