10bfc2ff1938669d1db89e4eeb0e975ff2a017bc
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-14 20:55:43 -0400

    extracting url from json url

--- gappengine/main.py
+++ gappengine/main.py
@@ -48,9 +48,9 @@ def getData():
     		json_obj = json.loads(x).get("mydata", "NOTHING")
     		print("RAW DATA: %s" % json_obj["url"])
     		print("MY DATA: %s" % ( json.loads(x).get("mydata", "NOTHING") ))
-            if json_obj is not "NOTHING":
-                url = json_obj["url"]
-                print("URL: %s" % ( url ))
+           # if json_obj is not "NOTHING":
+            url = json_obj["url"]
+            print("URL: %s" % ( url ))
     	print("LOST DATA: %s" % (x))
 
 
