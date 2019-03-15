f1ba4a5d4bd3cadd08b45e7da5fdd64adc9dae9a
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-14 21:07:41 -0400

    extracting url from json url

--- gappengine/main.py
+++ gappengine/main.py
@@ -10,18 +10,18 @@ from flask_cors import cross_origin
 app = Flask(__name__)
 
 def support_jsonp(f):
-    """Wraps output to JSONP"""
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        result = jsonify(f(*args, **kwargs))
-        callback = request.args.get('callback', False)
-        if callback:
-            content = str(callback) + '(' + str(result.data) + ')'
-            return current_app.response_class(content,
-                                              mimetype='application/json')
-        else:
-            return result
-    return decorated_function
+	"""Wraps output to JSONP"""
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		result = jsonify(f(*args, **kwargs))
+		callback = request.args.get('callback', False)
+		if callback:
+			content = str(callback) + '(' + str(result.data) + ')'
+			return current_app.response_class(content,
+											  mimetype='application/json')
+		else:
+			return result
+	return decorated_function
 
 @app.route('/')
 @cross_origin(send_wildcard=True)
@@ -40,25 +40,25 @@ def curated():
 @app.route('/_getData', methods=['GET', 'POST'])
 @cross_origin(send_wildcard=True)
 def getData():
-    #hit the data, package it, put it into json.
-    #ajax would have to hit this every so often to get latest data.
-    data = request.get_json()
-    url = ""
-    for x in request.args:
-    	if "mydata" in x:
-    		json_obj = json.loads(x).get("mydata", "NOTHING")
-            url = json_obj["url"]
-    		print("RAW DATA: %s" % json_obj["url"])
-    		print("MY DATA: %s" % ( json.loads(x).get("mydata", "NOTHING") ))
-    	print("LOST DATA: %s" % (x))
+	#hit the data, package it, put it into json.
+	#ajax would have to hit this every so often to get latest data.
+	data = request.get_json()
+	url = ""
+	for x in request.args:
+		if "mydata" in x:
+			json_obj = json.loads(x).get("mydata", "NOTHING")
+			url = json_obj["url"]
+			print("RAW DATA: %s" % json_obj["url"])
+			print("MY DATA: %s" % ( json.loads(x).get("mydata", "NOTHING") ))
+		print("LOST DATA: %s" % (x))
 
-    mycallback = request.args["callback"]
-    print("callback: %s" % (mycallback))
-    arr = {}
-    arr["blah"] = []
-    arr["blah"].append("stuff")
-    print("URL: " % (url))
-    return jsonify(response=arr)
+	mycallback = request.args["callback"]
+	print("callback: %s" % (mycallback))
+	arr = {}
+	arr["blah"] = []
+	arr["blah"].append("stuff")
+	print("URL: " % (url))
+	return jsonify(response=arr)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+	app.run(host='.0.0.1', port=8080, debug=True)
