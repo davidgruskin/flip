a59bf64d946027bd0c74c52538370f7c4fbe1259
Victor Mutai <Victor.Mutai@logmein.com>
2019-03-18 18:22:51 -0400

    added nlp code

--- gappengine/main.py
+++ gappengine/main.py
@@ -2,64 +2,32 @@ from functools import wraps
 from flask import Flask, request, current_app, jsonify
 import json
 from flask_jsonpify import jsonify
-
-
-# pip install -U flask-cors
 from flask_cors import cross_origin
 
-app = Flask(__name__)
+from flip_nlp import *
 
-def support_jsonp(f):
-    """Wraps output to JSONP"""
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        result = jsonify(f(*args, **kwargs))
-        callback = request.args.get('callback', False)
-        if callback:
-            content = str(callback) + '(' + str(result.data) + ')'
-            return current_app.response_class(content,
-                                              mimetype='application/json')
-        else:
-            return result
-    return decorated_function
+app = Flask(__name__)
 
 @app.route('/')
 @cross_origin(send_wildcard=True)
 def hello():
-	return jsonify({'message': 'This is just a test!'})
+	print("hello: request received")
+	return jsonify({'results': {'success': 1, 'message': 'Welcome to Flip' } })
 
-@app.route('/curated', methods=['POST'])
+@app.route('/curated', methods=['GET', 'POST'])
 @cross_origin(send_wildcard=True)
 def curated():
 	print("curated: request received")
 	data = request.get_json()
-	print(data)
-	return "%s({'a':1, 'b':2 })" % _GET_PARAMS('callback')
-	#return jsonify({'message': 'success returned!'})
+	url = data["url"]
+	triggers = data["triggers"]
 
-@app.route('/_getData', methods=['GET', 'POST'])
-@cross_origin(send_wildcard=True)
-def getData():
-    #hit the data, package it, put it into json.
-    #ajax would have to hit this every so often to get latest data.
-    data = request.get_json()
-    url = ""
-    for x in request.args:
-    	if "mydata" in x:
-    		json_obj = json.loads(x).get("mydata", "NOTHING")
-    		print("RAW DATA: %s" % json_obj["url"])
-    		url = json_obj["url"]
-    		print("MY DATA: %s" % ( json.loads(x).get("mydata", "NOTHING") ))
-    	print("LOST DATA: %s" % (x))
+	results = yhmh_nlp(url, triggers)
+	if results is "":
+		return jsonify({'results': {'success': 0, 'message': 'no recommendation' } })
 
-    mycallback = request.args["callback"]
-    print("callback: %s" % (mycallback))
-    arr = {}
-    arr["blah"] = []
-    arr["blah"].append("stuff")
-    print(url)
-    print("END")
-    return jsonify(response=arr)
+	print(results)
+	return jsonify({'results': {'success': 1, 'message': results } })
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+	app.run(host='127.0.0.1', port=8080, debug=True)
